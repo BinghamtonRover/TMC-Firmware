52eb4ea8dc6b11da9829736d4a93cfc15b201024
Levi Lesches <levilesches@gmail.com>
2024-05-07 21:12:44 -0400

    Refactored StepperMotor

--- BURT_TMC.cpp
+++ BURT_TMC.cpp
@@ -1,147 +1,146 @@
-/*! @file 
- * \addtogroup Motor 
- * \{
-*/
-
 #include "BURT_TMC.h"
 
 const int blockDelay = 10;  // ms
 
-// ================== StepperMotor ==================
-
-StepperMotor::StepperMotor(StepperMotorPins pins, StepperMotorConfig config, LimitSwitch limitSwitch) : 
-	pins(pins),
-	config(config),
-	limitSwitch(limitSwitch),
-	driver(TMC5160Stepper(SPI, pins.chipSelect, 0.075)) { }
-
-void StepperMotor::presetup() {
-	if (!IS_CONNECTED) return;
-	pinMode(pins.chipSelect, OUTPUT);
-	digitalWrite(pins.chipSelect, HIGH);
+bool LimitSwitch::isPressed() {
+  return pin != -1 && digitalRead(pin) == triggeredValue;
 }
 
-void StepperMotor::setup() {
-	if (!IS_CONNECTED) return;
-	Serial.print("Initializing motor " + config.name + "... ");
+StepperMotor::StepperMotor(StepperMotorPins pins, StepperMotorConfig config) : 
+  pins(pins),
+  config(config),
+  driver(TMC5160Stepper(SPI, pins.chipSelect, 0.075))
+  { }
 
-	pinMode(pins.enable, OUTPUT);
-	digitalWrite(pins.enable, LOW);
-	if (limitSwitch.pin != -1) pinMode(limitSwitch.pin, INPUT_PULLUP);
+StepperMotor::StepperMotor(StepperMotorPins pins, StepperMotorConfig config, LimitSwitch limitSwitch) :
+  pins(pins),
+  config(config),
+  limitSwitch(limitSwitch),
+  driver(TMC5160Stepper(SPI, pins.chipSelect, 0.075))
+  { }
 
-	driver.begin();
-	driver.reset();
-	TMC5160Stepper::IOIN_t ioin{ driver.IOIN() };
-	if (ioin.version == 0xFF || ioin.version == 0) {
-    Serial.println("\nDriver communication error on motor: " + config.name);
-    while(true);
-	} else if (ioin.sd_mode) {
-    Serial.println("\nMotor is configured for Step & Dir mode: " + config.name);
-    while(true);
-	} else if (ioin.drv_enn) {
-    Serial.println("Motor is not hardware enabled: " + config.name);
-    while(true);
-	}
+bool StepperMotor::isMoving() {
+  return driver.XTARGET() != driver.XACTUAL();
+}
 
-	// TODO: Decide if everything below this is needed: 
-	// See https://github.com/BinghamtonRover/arm-firmware/issues/6
-	digitalWrite(pins.enable, HIGH);  // disable driver to clear the cache
+int StepperMotor::currentSteps() {
+  return driver.XACTUAL() + limitSwitch.offset;
+}
+
+int StepperMotor::targetSteps() {
+  return driver.XTARGET() + limitSwitch.offset;
+}
+
+double StepperMotor::currentPosition() {
+  return currentSteps() * config.stepsPerUnit;
+}
+
+double StepperMotor::targetPosition() {
+  return targetSteps() * config.stepsPerUnit;
+}
+
+void StepperMotor::presetup() {
+  pinMode(pins.chipSelect, OUTPUT);
+  digitalWrite(pins.chipSelect, HIGH);
+}
+
+void StepperMotor::reset_driver() {
+  driver.begin();
+  driver.reset();
+  digitalWrite(pins.enable, HIGH);  // disable driver to clear the cache
 	delay(1000);
 	digitalWrite(pins.enable, LOW);   // re-enable drive, to start loading in parameters
+}
 
-	driver.GSTAT(7);
+void StepperMotor::check_driver() {
+  TMC5160Stepper::IOIN_t ioin { driver.IOIN() };
+  if (ioin.version == 0xFF || ioin.version == 0) {
+    Serial.print("\nDriver communication error on motor: ");
+    Serial.println(config.name);
+    while (true);
+  } else if (ioin.sd_mode) {
+    Serial.println("Motor is configured for Step & Direction mode: ");
+    Serial.println(config.name);
+    while (true);
+  } else if (ioin.drv_enn) {
+    Serial.println("Motor is not hardware enabled: ");
+    Serial.println(config.name);
+    while (true);
+  }
+}
+
+void StepperMotor::write_settings() {
+  // TODO: Decide if everything below this is needed: 
+	// See https://github.com/BinghamtonRover/arm-firmware/issues/6
+  driver.GSTAT(7);
 	driver.rms_current(config.current);
 	driver.tbl(2);
 	driver.toff(9);
 	driver.pwm_freq(1);
-	driver.a1(config.accel);
+	driver.a1(config.acceleration);
 	driver.v1(config.speed);
-	driver.AMAX(config.accel);
+	driver.AMAX(config.acceleration);
 	driver.VMAX(config.speed);
-	driver.DMAX(config.accel);
-	driver.d1(config.accel);
+	driver.DMAX(config.acceleration);
+	driver.d1(config.acceleration);
 	driver.vstop(100);
 	driver.vstart(100);
 	driver.RAMPMODE(0);
+}
 
-	Serial.println("Done!");
+void StepperMotor::setup() {
+  Serial.print("Initiializing motor...");
+  pinMode(pins.enable, OUTPUT);
+  digitalWrite(pins.enable, LOW);
+  if (limitSwitch.pin != -1) pinMode(limitSwitch.pin, INPUT_PULLUP);
+  reset_driver();
+  check_driver();
+  write_settings();
+  Serial.println("Done!");
+}
+
+void StepperMotor::calibrate() {
+  if (limitSwitch.pin == -1) return;
+  while (!limitSwitch.isPressed()) {
+    moveBySteps(10 * limitSwitch.direction);
+  }
+  stop();
+  int limitSteps = limitSwitch.position * config.stepsPerUnit;
+  limitSwitch.offset = limitSteps - driver.XACTUAL() * limitSwitch.direction;
 }
 
 void StepperMotor::update() {
-	if (!IS_CONNECTED) return;
-	bool isMovingTowardsSwitch = limitSwitch.direction > 0
-		? driver.XTARGET() > driver.XACTUAL()
-		: driver.XTARGET() < driver.XACTUAL();
-	if (isLimitSwitchPressed() && isMovingTowardsSwitch) stop();
+  int target = driver.XTARGET();
+  int current = driver.XACTUAL();
+  bool isMovingTowardsLimit = limitSwitch.direction > 0
+    ? target > current : target < current;
+  if (limitSwitch.isPressed() && limitSwitch.isBlocking && isMovingTowardsLimit) stop();
 }
 
 void StepperMotor::stop() {
-	if (!IS_CONNECTED) return;
-	driver.XTARGET(driver.XACTUAL());
-}
-
-void StepperMotor::calibrate() { 
-	if (limitSwitch.pin == -1 || !IS_CONNECTED) return;
-
-	int steps = driver.XACTUAL();
-	while(!isLimitSwitchPressed()) {
-		steps += 10 * limitSwitch.direction;
-		driver.XTARGET(steps);
-	}
-	if (!isLimitSwitchPressed()) return calibrate();
-	stop();  // the while loop overshoots the limit switch and will keep going
-	limitSwitch.offset = driver.XACTUAL();
-}
-
-void StepperMotor::moveTo(float position) {
-	moveBy(position - getPosition());
-}
-
-void StepperMotor::moveBy(float distance) {
-	// When called by [moveTo], this will be the same as the [position] parameter.
-	float targetPosition = getPosition() + distance;
-	// Check bounds
-	if (targetPosition > config.maxLimit || targetPosition < config.minLimit) return; 
-
-	int steps = (distance * config.toSteps);
-	int targetStep = config.isPositive
-		? driver.XACTUAL() + steps
-		: driver.XACTUAL() - steps;
-	if (!IS_CONNECTED) return;
-	driver.XTARGET(targetStep);
-}
-
-void StepperMotor::moveToSteps(int steps) {
-	steps += limitSwitch.offset;
-	moveBySteps(steps - driver.XACTUAL());
-}
-
-void StepperMotor::moveBySteps(int steps) {
-	if (!IS_CONNECTED) return; 
-	driver.XTARGET(driver.XACTUAL() + steps);
-}
-
-bool StepperMotor::isMoving() { 
-	if (!IS_CONNECTED) return false;
-	return driver.XTARGET() != driver.XACTUAL(); 
-}
-
-bool StepperMotor::isLimitSwitchPressed() {
-	if (limitSwitch.pin == -1 || !IS_CONNECTED) return false;
-	else return digitalRead(limitSwitch.pin) == limitSwitch.triggeredValue;
-}
-
-float StepperMotor::getPosition() {
-	return (driver.XACTUAL() - limitSwitch.offset) * config.toUnits;
-}
-
-float StepperMotor::getTarget() {
-	return (driver.XTARGET() - limitSwitch.offset) * config.toUnits;
+  driver.XTARGET(driver.XACTUAL());
 }
 
 void StepperMotor::block() {
-	while (isMoving()) delay(blockDelay);
+  while (isMoving()) delay(blockDelay);
 }
 
-// The following close bracket marks the file for Doxygen
-/*! \} */
+void StepperMotor::moveTo(double position) {
+  if (position > limitSwitch.maxLimit || position < limitSwitch.minLimit) return;
+  int steps = position * config.stepsPerUnit;
+  moveToSteps(steps);
+}
+
+void StepperMotor::moveBy(double offset) {
+  int steps = offset * config.stepsPerUnit;
+  moveBySteps(steps);
+}
+
+void StepperMotor::moveToSteps(int steps) {
+  driver.XTARGET(steps);
+}
+
+void StepperMotor::moveBySteps(int steps) {
+  int target = driver.XACTUAL() + steps;
+  driver.XTARGET(target);
+}
--- BURT_TMC.h
+++ BURT_TMC.h
@@ -1,173 +1,72 @@
-/*! @file 
- * \addtogroup Motor 
- * \{
-*/
-
-#ifndef burt_arm_motor_h
-#define burt_arm_motor_h
-
+#pragma once
 #include <Arduino.h>
-
 #include "TmcStepper.h"
 
-/// If this is false, all firmware will print to the console instead of using hardware.
-#define IS_CONNECTED true
+const double pi = 3.141592653589793;
+const int microstepsPerStep = 256;
+const int stepsPerRotation = 200;
+const int degreesPerRotation = 360;
+const double radiansPerRotation = 2 * pi;
 
-/// The pins that a #StepperMotor.
+const double stepsPerRadian = microstepsPerStep * stepsPerRotation / radiansPerRotation;
+const double stepsPerDegree = microstepsPerStep * stepsPerRotation / degreesPerRotation;
+
 struct StepperMotorPins {
-	/// The SPI chip select pin for this stepper motor.
-	int chipSelect;
-
-	/// The enable pin for this stepper motor. 
-	/// 
-	/// TMC5160 chips are enable-low, which means this must be written LOW to work.
-	int enable;
+  int enable;
+  int chipSelect;
 };
 
-/// Configuration for a #StepperMotor.
 struct StepperMotorConfig {
-	/// The name of this motor. Used for debugging.
-	String name;
-
-	String unitName = "radians";
-
-	/// The Root Mean Square current to feed the motor, in mA.
-	int current;
-
-	/// The speed of the motor, units unknown.
-	float speed;
-
-	/// The magnitude of the acceleration of the motor, units unknown.
-	float accel;
-
-	/// The minimum angular position, in radians. Leave blank for non-limited motion.
-	float minLimit = -INFINITY;
-
-	/// The maximum angular position, in radians. Leave blank for non-limited motion.
-	float maxLimit = INFINITY;
-
-	bool isPositive;
-
-	float gearboxRatio = 1.0;
-
-	float conversionFactor = 200 * 256 * gearboxRatio;
-
-	float toUnits = (2*PI) / (conversionFactor);
-
-	float toSteps = conversionFactor / (2*PI);
+  String name;
+  int current;
+  int speed;
+  int acceleration;
+  double stepsPerUnit;
 };
 
-/// Calibrates a stepper motor.
-/// 
-/// A limit switch (or sensor) serves to calibrate a stepper motor by allowing it to move to a known
-/// #position after powering on.
 struct LimitSwitch {
-	/// The pin this switch or sensor is connected to.
-	int pin = -1;
+  int pin = -1;
+  int triggeredValue;
+  int direction = 1;
+  bool isBlocking = true;
+  double position;
+  double minLimit = -INFINITY;
+  double maxLimit = INFINITY;
 
-	/// The value that the #pin should read if the switch or sensor is activated.
-	int triggeredValue;
-
-	/// Whether to approach this switch or sensor by moving positive (+1) or negative (-1) steps.
-	int direction;
-
-	/// The position this switch or sensor is at, in terms of #StepConfig::stepsToPosition. For example,
-	/// a rotating arm might have a limit switch at 90 degrees (or pi / 2 rad). This field is useful
-	/// when placing a limit switch at #StepperMotorConig::minLimit or #StepperMotorConfig::maxLimit.
-	float position;
-
-	int offset = 0;
+  int offset = 0;
+  bool isPressed();
 };
 
-/// Controls a TMC 5160 stepper motor. 
-/// 
-/// - In `setup`, call #presetup for *every* motor you have, *then* call #setup on each
-/// - After a motor has been #setup, call #calibrate on it. Does nothing with no limit switch
-/// - In `loop`, call #update for each motor you have
-class StepperMotor { 
-	private: 
-		/// The pins this motor is attached to.
-		StepperMotorPins pins;
-
-		/// The configuration for this stepper motor.
-		StepperMotorConfig config;
-
-		/// The limit sensor for this motor. Leave blank if none are attached.
-		LimitSwitch limitSwitch;
-
-		/// The TMC instance for this motor, backed by the `TMCStepper` library.
+class StepperMotor {
+  private: 
+    StepperMotorPins pins;
+    StepperMotorConfig config;
+    LimitSwitch limitSwitch;
 		TMC5160Stepper driver;
 
-	public: 
-		/// Manage a stepper motor with the given configuration.
-		StepperMotor(StepperMotorPins pins, StepperMotorConfig config, LimitSwitch limitSwitch);
+    void reset_driver();
+    void check_driver();
+    void write_settings();
 
-		/// Ensures this stepper motor will not interfere with other motors.
-		/// 
-		/// You must call this method on *every* motor before trying to interface with one of them.
-		void presetup();
+  public: 
+    StepperMotor(StepperMotorPins pins, StepperMotorConfig config);
+    StepperMotor(StepperMotorPins pins, StepperMotorConfig config, LimitSwitch limitSwitch);
 
-		/// Initializes the motor. Must be called *after* #presetup is called on *all* motors.
-		/// 
-		/// The motor needs to be fully powered and on for this function to work. If setup fails, this
-		/// function will log to the serial monitor and block indefinitely.
-		void setup();
+    bool isMoving();
+    int currentSteps();
+    int targetSteps();
+    double currentPosition();
+    double targetPosition();
 
-		/// Calibrates the motor.
-		/// 
-		/// If there is a limit switch (`limitSwitch.pin != -1`):
-		/// - Moves toward the limit switch (#LimitSwitch::direction) until #isLimitSwitchPressed is `true`
-		/// - Set #stepsAtLimitSensor to the current position of the motor (`driver.XACTUAL`)
-		/// 
-		/// Since this is usually in `setup` (where `loop` is unavailable) this function handles checking
-		/// the limit switch and stopping by itself in a while loop, which means it is blocking.
-		void calibrate();
+    void presetup();
+    void setup();
+    void calibrate();
+    void update();
+    void stop();
+    void block();
 
-		/// Maintains the state of the motor and searches for problems, calling #stop if needed.
-		void update();
-
-		/// Stops this motor by setting its target to its current position.
-		/// 
-		/// Call this in #update if the motor is passing its limits as defined by #StepperMotorConfig.
-		void stop();
-
-		/// Moves the motor to a specific angle, in radians. 
-		/// 
-		/// Differs from #moveBy in that this function receives a target rotation and rotates 
-		/// until it matches (see #isFinished). Use this method in IK mode, where you know where to go. 
-		void moveTo(float radians);
-
-		/// Moves the motor by a given rotation, in radians. 
-		/// 
-		/// Differs from #moveTo in that this function receives an offset in radians and rotates 
-		/// by that amount. Use this method in precision mode, where you move in small increments. 
-		void moveBy(float radians);
-
-		/// Moves to the given step position. 
-		/// 
-		/// Use in debugging only. In production code, use #moveTo. This method can help determine
-		/// when #StepperMotorConfig::stepsPer180 is off, or when the motor is misbehaving.
-		void moveToSteps(int destination);
-
-		/// Moves by the given amount of steps. 
-		/// 
-		/// Use in debugging only. In production code, use #moveBy. This method can help determine
-		/// when #StepperMotorConfig::stepsPer180 is off, or when the motor is misbehaving.
-		void moveBySteps(int steps);
-		
-		/// Whether this motor is still moving.
-		/// 
-		/// Compares #TMC5160Stepper::XACTUAL to #TMC5160Stepper::XTARGET. 
-		bool isMoving();
-
-		/// Whether the limit switch is being pressed.
-		bool isLimitSwitchPressed();
-
-		float getPosition();
-		float getTarget();
-		void block();
+    void moveTo(double position);
+    void moveBy(double offset);
+    void moveToSteps(int steps);
+    void moveBySteps(int steps);
 };
-
-#endif
-// The following close bracket marks the file for Doxygen
-/*! \} */
